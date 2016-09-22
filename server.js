36fb4672ae88168feba1faf0603d94af3ef0d70b
DeemaJS <deeema@outlook.com>
2016-09-22 18:24:18 +0300

    Add debugger

--- server.js
+++ server.js
@@ -3,17 +3,23 @@ var http = require('http');
 var url = require('url');
 
 
-var server = new http.Server(function(req, res) {
+var server = http.createServer();
 
-  var urlParsed = url.parse(req.url, true);
 
-  if (urlParsed.pathname == '/echo' && urlParsed.query.message) {
+server.on('request', function(req, res) {
+
+    var urlParsed = url.parse(req.url, true);
+    debugger;
+
+  if (req.method == 'GET' && urlParsed.pathname == '/echo' && urlParsed.query.message) {
     res.setHeader('Cache-control', 'no-cache,no-store,must-revalidate');
     res.end( urlParsed.query.message );
-  } else {
-    res.statusCode = 404; // Not Found
-    res.end("Page not found");
+    return;
   }
+  
+  res.statusCode = 404; // Not Found
+  res.end("Page not found");  
 });
 
-server.listen(1337, '127.0.0.1');
+server.listen(1337);
+console.log('Server running');
