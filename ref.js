a23c4d4797e64562556ca2d79131850dc2c2da6a
DeemaJS <deeema@outlook.com>
2016-09-24 17:56:49 +0300

    nextTick, setImmediate

--- ref.js
+++ ref.js
@@ -7,12 +7,11 @@ var server = new http.Server(function(req, res) {
 }).listen(3000);
 
 setTimeout(function() {
-  server.close(function() {
-  	clearInterval(timer);
-  });
+  server.close();
 }, 2500);
 
 var timer = setInterval(function() {
   console.log(process.memoryUsage());
 }, 1000);
 
+timer.unref();
