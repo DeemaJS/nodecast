0f57d37e6d462267137917cc85f6c12fb0c27477
DeemaJS <deeema@outlook.com>
2016-10-09 16:40:46 +0300

    Js type array

--- types.js
+++ types.js
@@ -1,3 +1,6 @@
 console.log(![1]); // boolean
 console.log(+[] === 0); // number
-console.log([] === ""); // string
+console.log([] + [] === ""); // string
+
+console.log("deema"[1] === "e");
+console.log(+("2" + "1") === 21);
