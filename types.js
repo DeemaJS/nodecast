610505d59cccbcdd13cff4763a7def33f42be37f
DeemaJS <deeema@outlook.com>
2016-10-13 12:53:12 +0300

    Add type feature

--- types.js
+++ types.js
@@ -7,3 +7,7 @@ console.log(+("2" + "1") === 21);
 console.log(+!![] === 1);
 console.log((![]+[])[+!![]] === "a");
 console.log([][[]] + [] === "undefined");
+
+console.log([2,1,7]["sort"]());
+console.log((12)["toString"](2) === "1100");
+console.log((35)["toString"](36) === "z");
